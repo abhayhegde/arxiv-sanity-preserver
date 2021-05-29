5aca452bf35e0fe4ed04d69414b530112a03a99d
abhayhegde <abhayhegde.skp@gmail.com>
2021-05-29 15:07:10 +0530

    added bash

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo apt-get install python3.8
+sudo apt-get install imagemagick poppler-utils
+pip install -r requirements.txt
 python fetch_papers.py
 python download_pdfs.py
 python parse_pdf_to_text.py
