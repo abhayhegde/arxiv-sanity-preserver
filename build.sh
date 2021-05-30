b0771c2b5795b37e4e921e96873644486a234ff9
abhayhegde <abhayhegde.skp@gmail.com>
2021-05-30 17:29:49 +0530

    updated pdf thumbnails code

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 python -m venv env/
+source env/bin/activate
 pip install -r requirements.txt
+sqlite3 as.db < schema.sql
 python fetch_papers.py
 python download_pdfs.py
 python parse_pdf_to_text.py
-python thumb_pdf.py
+python pdf_thumbs.py
 python analyze.py
 python buildsvm.py
 python make_cache.py
+sudo service mongod start
+python serve.py --prod --port 80
